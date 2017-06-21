d2f16f330828b6115b471e2f2f1ba80aacf66f5b
corey redding and justin gomez <corey@>
2017-06-21 09:50:08 -0700

    disable button for 1 second during if statement. clean up unused animations. add random animation function. display none to animations. display animations on button trigger

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,20 @@ $(document).ready(function() {
   $('.hamburger').click(function () {
     // get random number from 1 to (number of jokes)
     var random = getRandomArbitrary(1, 24);
+    var randomAnime = getRandomArbitrary(1, 3);
     if ($(this).hasClass('hamburger-active')) {
       $(this).removeClass('hamburger-active'); //keeps track of state
       $('.question').hide();
       $('.answer').show('slide', {direction: 'left'}, 1000);
-      $('.headtop').removeClass('head-animation'); //remove class so head can open again
+      $('.headtop').removeClass('head-open'); //remove class so head can open again
+      $('.animation' + randomAnime).show();
+      $('.hamburger').addClass("unclickable"); //disable hamburger button for 1.2 seconds
+      setTimeout(function(){
+        $('.hamburger').removeClass('unclickable');
+      }, 1200); //end of disable hamurger function
     } else {
       $(this).addClass('hamburger-active'); //keeps track of state
-      $(".roll-img").removeClass("tp-animation");  //hide tp
+      $(".roll-img").removeClass("tp-activate");  //hide tp
       $('[class*="joke"]').hide(); //hide all old jokes
       $('.joke' + random).show();
       $('.tp-sheet').show(); //white box hiding joke appears
@@ -23,14 +29,14 @@ $(document).ready(function() {
       $('.hamburger').addClass("unclickable"); //disable hamburger button for 5.2 seconds
       setTimeout(function(){
         $('.hamburger').removeClass('unclickable');
-      }, 5200);
+      }, 5200); //end of disable hamurger function
       $('.answer').hide();
       $('.question').show('slide', {direction: 'left'}, 1000);
-      $('.headtop').addClass('head-animation'); //open head
+      $('.headtop').addClass('head-open'); //open head
       setTimeout(function(){
-          $(".roll-img").addClass("tp-animation"); //tp flys out of head after 1 second
+          $(".roll-img").addClass("tp-activate"); //tp flys out of head after 1 second
       }, 1000);
+      $("[class*='animation']").hide(); //hide all animations
     }
   });
-
 });
