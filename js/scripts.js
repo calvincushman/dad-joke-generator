e87c42984630be01cbbe935d0cde021387678af2
Daniel <dlopez6877@gmail.com>
2017-06-20 23:36:04 -0700

    reposition face h1 and jokes to be lower on screen. set timeout to disable button and rename class hamburger active to unclickable. adjust toilet paper animation to fit repositioning

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ function getRandomArbitrary(min, max) {
   return Math.round(Math.random() * (max - min)) + min;
 }
 
+var globalState = false;
+
 $(document).ready(function() {
   $('.hamburger').click(function () {
     var random = getRandomArbitrary(1, 24)
@@ -10,17 +12,18 @@ $(document).ready(function() {
       $('.question').hide();
       $('.answer').show('slide', {direction: 'left'}, 1000);
       $('.headtop').removeClass('head-animation');
-
-      setTimeout(function(){
-          $(".roll-img").removeClass("tp-animation");
-      }, 3000);
     } else {
       $(this).addClass('hamburger-active');
+      $(".roll-img").removeClass("tp-animation");
       $('[class*="joke"]').hide();
       $('.joke' + random).show();
       $('.tp-sheet').show();
       setTimeout(function(){
-          $('.tp-sheet').hide('slide', {direction: 'right'}, 1000);
+        $('.tp-sheet').hide('slide', {direction: 'right'}, 1000);
+      }, 5200);
+      $('.hamburger').addClass("unclickable");
+      setTimeout(function(){
+        $('.hamburger').removeClass('unclickable');
       }, 5200);
       $('.answer').hide();
       $('.question').show('slide', {direction: 'left'}, 1000);
