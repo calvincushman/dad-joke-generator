a886e9dee50cf777dab40353be15686cac8bd78c
calvin cushman and daniel lopez <Guest@Epicodus-1D.local>
2017-06-22 13:42:07 -0700

    edit steak animation center text in joke on mobile landscape

---
+++ README.md
@@ -0,0 +1,60 @@
+# _DAD-JOKE-GENERATOR_
+
+## CONTRIBUTORS: **Daniel Lopez, Corey Redding, Calvin Cushman, Justin Gomez**
+
+### PROJECT DESCRIPTION
+
+1. A WEB APPLICATION THAT GENERATES DAD JOKES
+2. ANIMATIONS ARE TRIGGERED WHEN APP LOADS AND DAD JOKES ARE GENERATED
+3. THE PAGE IS RESPONSIVE TO 3 DIFFERENT SCREEN SIZES
+
+# JS SPECS
+* get random number from 1 to (number of jokes)
+
+* get random number from 1 to (number of animations)
+
+* keeps track of state
+
+* remove class so head can open again
+
+* generate random animation
+
+* disable hamburger button for 1.2 seconds
+
+* end of disable hamburger function
+
+* keeps track of state
+
+* hide tp
+
+* hide all old jokes
+
+* white box hiding joke appears
+
+* white box hiding joke slides away after 5.2 seconds
+
+* disable hamburger button for 5.2 seconds
+
+* end of disable hamburger function
+
+* open head
+
+* tp flies out of head after 1 second
+
+* hide all animations
+
+### TECHNOLOGIES USED
+* HTML
+* SASS
+* JAVASCRIPT
+* JQUERY
+* GIT/GITHUB
+* CODE-PEN
+
+### COMPLETE SET-UP INSTRUCTIONS
+1. DOWNLOAD OR CLONE FROM GITHUB REPOSITORY [Click here](https://github.com/calvincushman/dad-joke-generator) ...
+2. TO CLONE, TYPE: git clone https://github.com/calvincushman/dad-joke-generator INTO YOUR TERMINAL AND PRESS "ENTER"
+3. OPEN UP THE FOLDER "dad-joke-generator"
+4. DOUBLE-CLICK **index.html**
+
+* Copyright (c) 2017, **_{Daniel Lopez, Corey Redding, Calvin Cushman, Justin Gomez}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//generates random number between two floating points
 function getRandomArbitrary(min, max) {
   return Math.round(Math.random() * (max - min)) + min;
 }
@@ -19,7 +20,7 @@ $(document).ready(function() {
       $('.hamburger').addClass("unclickable"); //disable hamburger button for 1.2 seconds
       setTimeout(function(){
         $('.hamburger').removeClass('unclickable');
-      }, 1200); //end of disable hamurger function
+      }, 1200); //end of disable hamburger function
     } else {
         $('body').append("<audio autoplay class='audio'>" + "<source src='badum/fart-05.mp3' type='audio/mpeg'>" + "</audio>");
       $(this).addClass('hamburger-active'); //keeps track of state
@@ -33,12 +34,12 @@ $(document).ready(function() {
       $('.hamburger').addClass("unclickable"); //disable hamburger button for 5.2 seconds
       setTimeout(function(){
         $('.hamburger').removeClass('unclickable');
-      }, 5200); //end of disable hamurger function
+      }, 5200); //end of disable hamburger function
       $('.answer').hide();
       $('.question').show('slide', {direction: 'left'}, 1000);
       $('.headtop').addClass('head-open'); //open head
       setTimeout(function(){
-          $(".roll-img").addClass("tp-activate"); //tp flys out of head after 1 second
+          $(".roll-img").addClass("tp-activate"); //tp flies out of head after 1 second
       }, 1000);
       $("[class*='animation']").hide(); //hide all animations
     }
